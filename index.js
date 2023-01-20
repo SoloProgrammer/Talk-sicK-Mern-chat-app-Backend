6ed4aec6e868127b764e1c81229f9656ebab3137
pratham <prathamshinde987@gmail.com>
2023-01-21 03:46:16 +0530

    small changes1

--- index.js
+++ index.js
@@ -24,4 +24,26 @@ app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
 app.use('/api/message', messageRoutes)
 
-app.listen(port, () => console.log(` ChatApp Backend is running on server...${port} `))
+const server = app.listen(port, () => console.log(` ChatApp Backend is running on server...${port} `))
+
+const io = require('socket.io')(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("connected to socket.io");
+
+    socket.on('setup', (userData) => {
+        socket.join(userData._id);
+        console.log(userData._id);
+        socket.emit("connected")
+    })
+
+    socket.on('join chat', (chatRoom) => {
+        socket.join(chatRoom)
+        console.log("User Joined chatRoom: ", chatRoom)
+    })
+})
