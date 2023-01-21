c852752a8181ffd83120a676b4ac44c035c7a27d
unknown <SoloProgrammer>
2023-01-21 20:18:57 +0530

    added transports and polling option in socket congf

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api/message', messageRoutes)
 const server = app.listen(port, () => console.log(` ChatApp Backend is running on server...${port} `))
 
 const io = require('socket.io')(server, {
-    transports: ['websocket'],
+    'transports': ['websocket', 'polling'],
     pingTimeout: 120000,
     cors: {
         origin: process.env.CLIENT || "http://localhost:3000",
