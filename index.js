17a9618b901f5d94464b57d33c5e6a100e7dcb78
unknown <SoloProgrammer>
2023-01-22 20:10:15 +0530

    refrershing chats of user who recieves new msgs

--- index.js
+++ index.js
@@ -62,24 +62,24 @@ try {
 
             if (!chat.users) return console.log("chat.users is not defined");
 
-            chat.users.forEach((user) => {
+            chat.users.forEach(async (user) => {
 
                 if (user._id == newMessageRecieved.sender._id) return
 
                 // refreshedChatswill be used for refreshing all of the chats of that user who recives a new message from another user...................!
-                // let refreshedChats = await Chat.find(
-                //     {
-                //         users: { $elemMatch: { $eq: user._id } }
-                //     })
-                //     .populate('users', '-password')
-                //     .populate('latestMessage')
-                //     .populate('groupAdmin', '-password')
-                //     .sort({ updatedAt: -1 })
+                let refreshedChats = await Chat.find(
+                    {
+                        users: { $elemMatch: { $eq: user._id } }
+                    })
+                    .populate('users', '-password')
+                    .populate('latestMessage')
+                    .populate('groupAdmin', '-password')
+                    .sort({ updatedAt: -1 })
 
-                //     refreshedChats = await User.populate(refreshedChats, {
-                //     path: "latestMessage.sender",
-                //     select: "name avatar email phone"
-                // })
+                    refreshedChats = await User.populate(refreshedChats, {
+                    path: "latestMessage.sender",
+                    select: "name avatar email phone"
+                })
 
                 socket.in(user._id).emit("message recieved", newMessageRecieved, Previousmessages, refreshedChats)
             });
